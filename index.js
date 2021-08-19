28e363d506929191a9cc58198b7a8eb4b6e05662
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-08-19 21:07:30 +0200

    Progress with today's exercises
    Now userboard is included but need to add event listeners to functions.
    In overall need to stop repeating myself and maybe userBoard functions re-usable for last exercise(?). Otherwise will clutter the index.js file.

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ let createBoard = function() {
   }
 }
 
+// CREATE USER BOARD:
+let createUserBoard = function() {
+  // CREATE CONTAINER:
+  let userBoardContainer = document.createElement("div");
+  userBoardContainer.id = "userBoard";
+  document.body.appendChild(userBoardContainer);
+  
+  // GET THE NEWLY CREATED CONTAINER ELEMENT:
+  let userBoard = document.getElementById("userBoard");
+
+  // ADD TITLE:
+  let userBoardTitle = document.createElement("h2");
+  userBoardTitle.innerText = "User board";
+  userBoardTitle.style.textAlign = "center";
+  userBoard.appendChild(userBoardTitle);
+
+  // ADD THE CELLS:
+  for(let i=1; i <= 24; i++) {
+    let userBoardCell = document.createElement("div");
+    userBoardCell.classList.add("boardCell");
+
+    let randomNumber = Math.ceil(Math.random() * 76);
+    userBoardCell.innerText = randomNumber;
+
+    userBoard.appendChild(userBoardCell);
+  }
+}
+
 // CREATE BUTTON:
 let createButton = function() {
   // Create a <button> element:
@@ -41,26 +69,86 @@ let createButton = function() {
   button.onclick = randomNumbers;
 }
 
+// CREATE RESET BUTTON:
+let resetButton = function() {
+  let buttonElement = document.createElement("button");
+  buttonElement.id = "resetBtn";
+  buttonElement.innerText = "Reset the board!";
+  document.body.appendChild(buttonElement);
+
+  let resetButton = document.getElementById("resetBtn");
+  resetButton.onclick = function() {
+    let boardElements = document.getElementsByClassName("boardCell");
+    for(let element of boardElements) {
+      element.classList.remove("selected");
+    }
+  }
+}
+
+// Maybe create re-usable user-board buttons. Repeating steps.
+// CREATE USERBOARD BUTTON:
+let userBoardButton = function() {
+  let userBoardBtnElement = document.createElement("button");
+  userBoardBtnElement.classList = "userBoardBtn";
+  userBoardBtnElement.innerText = "Random number!";
+  userBoardBtnElement.display = "inline-block";
+
+  let userBoardBtnContainer = document.createElement("div");
+  userBoardBtnContainer.id = "userBoardBtnContainer"
+
+  let userBoard = document.getElementById("userBoard");
+  userBoard.appendChild(userBoardBtnContainer);
+  userBoardBtnContainer.appendChild(userBoardBtnElement);
+}
+
+// CREATE USERBOARD RESET BUTTON:
+let userBoardResetButton = function() {
+  let userBoardResetBtn = document.createElement("button");
+    userBoardResetBtn.classList = "userBoardResetBtn";
+    userBoardResetBtn.innerText = "Reset!";
+    userBoardResetBtn.display = "inline-block";
+
+  let userBoardBtnContainer = document.getElementById("userBoardBtnContainer");
+  userBoardBtnContainer.appendChild(userBoardResetBtn);
+}
+
+
 // Function to run when button clicked:
 let randomNumbers = function() {
   // Get all the board elements, which contain the number:
   let boardNumbers = document.getElementsByClassName("boardCell");
 
+  // Take Always a new number:
+  
+  // Loop through the container element full of elements with numbers:
+  for (let j = 0; j < boardNumbers.length; j++) {
   // Randomly select an element which contains the number between 1-76:
-  let selectedNumberElement = boardNumbers[Math.ceil(Math.random() * 76)];
-
-  // Add class 'selected' to the randomly selected element:
-  selectedNumberElement.classList.add("selected");
+  let selectedNumberEl = boardNumbers[Math.floor(Math.random() * 76)];
+  // If the number has class 'selected' - continue with next iteration:
+  if(selectedNumberEl.classList.contains("selected")) {
+      continue;
+    } else {
+      // Add class 'selected' to the randomly selected element:
+      selectedNumberEl.classList.add("selected");
+      // Break out from the loop!
+      // Otherwise this will carry on and go over every number
+      // and color the element!
+      break;
+    }
+  }
 }
 
 
 window.onload = function() {
   createBoard();
   createButton();
+  resetButton();
+  createUserBoard();
+  userBoardButton();
+  userBoardResetButton();
 }
 
 
 // EXTRA: 
-// Take ALWAYS a new number (eg. avoid randoming the number 10 3 times)
 // Create a USER BOARD with 24 randomized numbers that highlights as the main board does
 // Let the user choose HOW MANY user boards he's willing to play with and create them.
